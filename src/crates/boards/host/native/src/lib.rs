e0543286ea149e4bbe2f3a2c10955c01235e1d4d
pete-restall <pete@restall.net>
2025-05-02 14:53:37 +1200

    Found some lurking tabs.

--- src/crates/boards/host/native/src/lib.rs
+++ src/crates/boards/host/native/src/lib.rs
@@ -5,9 +5,9 @@ pub use smeg_mcu_host_native::main;
 
 #[cfg(test)]
 pub mod tests {
-	#[test]
-	pub fn an_example_test() {
-		// TODO: An example test to prove it works - remove when doing this for real...
-    	assert_eq!(42, 42);
-	}
+    #[test]
+    pub fn an_example_test() {
+        // TODO: An example test to prove it works - remove when doing this for real...
+        assert_eq!(42, 42);
+    }
 }
