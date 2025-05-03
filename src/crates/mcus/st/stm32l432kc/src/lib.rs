3d66712aa80249f531590f46f17bf7165567866c
pete-restall <pete@restall.net>
2025-05-03 21:45:11 +1200

    Renamed cortex_m4 to cortex_m4_family.

--- src/crates/mcus/st/stm32l432kc/src/lib.rs
+++ src/crates/mcus/st/stm32l432kc/src/lib.rs
@@ -11,6 +11,6 @@ mod blinky_blinky;
 
 // TODO: Eventually when proper symbols are used, this ought to be able to be deleted...
 pub fn needed_to_prevent_linker_gc() {
-    extern crate smeg_mcu_arm_cortex_m4;
-    smeg_mcu_arm_cortex_m4::needed_to_prevent_linker_gc();
+    extern crate smeg_mcu_arm_cortex_m4_family;
+    smeg_mcu_arm_cortex_m4_family::needed_to_prevent_linker_gc();
 }
