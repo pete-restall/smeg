dece5126aaaaa60ff79c4d23265a551bb8b5ea22
pete-restall <pete@restall.net>
2025-05-05 12:33:54 +1200

    Bugfix - tests can compile incompatible crates, which results in linker script generation and usage when running tests on the host.  Obviously this fails.  Added a simple test for cross-compilation to avoid generating a linker script in this scenario.

--- src/crates/mcus/st/stm32l432kc/build.rs
+++ src/crates/mcus/st/stm32l432kc/build.rs
@@ -10,8 +10,15 @@ use smeg_config::SMEG_CONFIG;
 static LINKER_SCRIPT_TEMPLATE: &str = include_str!("linker-script.lld.tt");
 
 fn main() -> ResultAnyError<()> {
+    let target_triple = std::env::var("TARGET").unwrap_or_default();
+    let host_triple = std::env::var("HOST").unwrap_or_default();
+    if target_triple == host_triple {
+        println!("Not compiling for an MCU target, so build script has nothing further to do.");
+        return Ok(());
+    }
+
     let smeg_out_dir = smeg_out_dir()?;
-    let linker_script_filename = format!("{smeg_out_dir}/linker-script.lld");
+    let linker_script_filename = format!("{smeg_out_dir}/smeg-os.lld");
 
     let mut fd = std::fs::File::create(&linker_script_filename)?;
     fd.write_all(create_linker_script_from(LINKER_SCRIPT_TEMPLATE)?.as_bytes())?;
