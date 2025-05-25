211613c337bc08880a6a73e7249cc4d02f32f725
pete-restall <pete@restall.net>
2025-05-25 17:20:22 +1200

    Minor change for alphabetic ordering.

--- src/crates/kernel/src/lib.rs
+++ src/crates/kernel/src/lib.rs
@@ -2,12 +2,6 @@
 
 pub mod bootstrapping;
 
-#[cfg(all(not(test), feature = "std"))]
-pub mod panic_handler;
-
-#[cfg(not(all(not(test), feature = "std")))]
-mod panic_handler;
-
 pub mod errors;
 
 #[cfg(target_pointer_width = "32")]
@@ -15,6 +9,12 @@ pub type HalfUsize = u16;
 
 #[cfg(target_pointer_width = "64")]
 pub type HalfUsize = u32;
+
+#[cfg(all(not(test), feature = "std"))]
+pub mod panic_handler;
+
+#[cfg(not(all(not(test), feature = "std")))]
+mod panic_handler;
 
 pub(crate) extern crate self as smeg_kernel;
 
