121113ad41ad15c1255d2078549a2302478651d0
pete-restall <pete@restall.net>
2025-05-28 16:23:54 +1200

    Some better names.

--- src/crates/kernel/tests/bss_section_initialiser_with_checks_test.rs
+++ src/crates/kernel/tests/bss_section_initialiser_with_checks_test.rs
@@ -17,18 +17,18 @@ fn fill_bss_section__called_with_start_after_past_end__expect_despair() {
     _fill_bss_section__called_with_start_after_past_end__expect_despair::<10, 7>();
 }
 
-fn _fill_bss_section__called_with_start_after_past_end__expect_despair<const BUF_SIZE: usize, const INDEX: usize>() {
+fn _fill_bss_section__called_with_start_after_past_end__expect_despair<const BSS_SIZE: usize, const INDEX: usize>() {
     expect!(|| unsafe {
-        let mut buf = [MaybeUninit::<usize>::uninit(); BUF_SIZE];
-        let (invalid_start, invalid_past_end) = invalid_ordering_split_at(INDEX, &mut buf);
+        let mut bss = [MaybeUninit::new(0_usize); BSS_SIZE];
+        let (invalid_start, invalid_past_end) = invalid_ordering_split_at(INDEX, &mut bss);
         BssSectionInitialiserWithChecks.fill_bss_section(invalid_start, invalid_past_end, any_fill());
     }).to_despair_with_error_code(KernelErrorCode::LinkerScriptDespair);
 }
 
-fn invalid_ordering_split_at(index: usize, buf: &mut [MaybeUninit<usize>]) -> (&mut MaybeUninit<usize>, &MaybeUninit<usize>) {
-    let (buf1, buf2) = buf.split_at_mut(index);
-    let past_end_before_start = &buf1[0];
-    let start_after_past_end = &mut buf2[0];
+fn invalid_ordering_split_at(index: usize, bss: &mut [MaybeUninit<usize>]) -> (&mut MaybeUninit<usize>, &MaybeUninit<usize>) {
+    let (head, tail) = bss.split_at_mut(index);
+    let past_end_before_start = &head[0];
+    let start_after_past_end = &mut tail[0];
     (start_after_past_end, past_end_before_start)
 }
 
