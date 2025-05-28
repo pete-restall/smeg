ab7d65930e4fc8e58feb0c62ec388523c152170d
pete-restall <pete@restall.net>
2025-05-28 20:08:11 +1200

    Disabled warnings about duplicate unsafes as a result of macro expansion.

--- src/crates/kernel/src/errors/despair.rs
+++ src/crates/kernel/src/errors/despair.rs
@@ -3,18 +3,21 @@ macro_rules! despair {
     (with($with:expr), because($($because:literal),+)) => {
         use ::smeg_kernel::errors::{error_tag, KernelError, TaggedError};
         unsafe extern "Rust" { unsafe fn __smeg_is_in_despair(squid: KernelError) -> !; }
+        #[allow(unused_unsafe)]
         unsafe { __smeg_is_in_despair(TaggedError::new($with, error_tag!($($because),+))); }
     };
 
     (with($with:expr)) => {
         use ::smeg_kernel::errors::{error_tag, KernelError, TaggedError};
         unsafe extern "Rust" { unsafe fn __smeg_is_in_despair(squid: KernelError) -> !; }
+        #[allow(unused_unsafe)]
         unsafe { __smeg_is_in_despair(TaggedError::new($with, error_tag!("In despair because of ", stringify!($with)))); }
     };
 
     (because($($because:literal),+)) => {
         use ::smeg_kernel::errors::{error_tag, KernelError, KernelErrorCode, TaggedError};
         unsafe extern "Rust" { unsafe fn __smeg_is_in_despair(squid: KernelError) -> !; }
+        #[allow(unused_unsafe)]
         unsafe { __smeg_is_in_despair(TaggedError::new(KernelErrorCode::GeneralDespair, error_tag!($($because),+))); }
     };
 }
