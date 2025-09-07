5ba9acadbc6f746804c0e8c61c33262639c06c2f
pete-restall <pete@restall.net>
2025-09-07 17:29:19 +1200

    Fixed whitespace.

--- src/crates/kernel/src/sync/mod.rs
+++ src/crates/kernel/src/sync/mod.rs
@@ -6,13 +6,13 @@ pub use no_lock::*;
 pub trait LockGuard<'protected, T> { }
 
 pub trait Lock<'protected, T> {
-	type Guard: LockGuard<'protected, T>;
+    type Guard: LockGuard<'protected, T>;
 
     fn lock(&'protected self) -> Result<Self::Guard>;
 }
 
 pub trait TryLock<'protected, T> {
-	type Guard: LockGuard<'protected, T>;
+    type Guard: LockGuard<'protected, T>;
 
     fn try_lock(&'protected self) -> Result<Self::Guard>;
 }
